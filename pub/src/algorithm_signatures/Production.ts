388ddee40a19427dc19b4a59d0cc86f510167e9b
Corno Schraverus <corno@schraverus.com>
2026-01-04 01:15:13 +0100

    .

--- pub/src/algorithm_signatures/Production.ts
+++ pub/src/algorithm_signatures/Production.ts
@@ -6,17 +6,17 @@ export type Production<Iterator_Element, Result, Error> = (
     abort: Abort<Error>,
 ) => Result
 
-export type Production_With_Parameters<Iterator_Element, Result, Error, Parameters> = (
+export type Production_With_Parameters<Result, Error, Iterator_Element, Parameters> = (
     iterator: Iterator<Iterator_Element>,
     abort: Abort<Error>,
     $p: Parameters,
 ) => Result
 
-export type Production_Without_Error<Iterator_Element, Result> = (
+export type Production_Without_Error<Result, Iterator_Element> = (
     iterator: Iterator<Iterator_Element>,
 ) => Result
 
-export type Production_Without_Error_With_Parameters<Iterator_Element, Result, Parameters> = (
+export type Production_Without_Error_With_Parameters<Result, Iterator_Element, Parameters> = (
     iterator: Iterator<Iterator_Element>,
     $p: Parameters,
 ) => Result
