cff255eed1a9a446694b6decee8045d1f51c0265
Corno Schraverus <corno@schraverus.com>
2026-01-07 17:26:33 +0100

    .

--- pub/src/interfaces/Iterator.ts
+++ pub/src/interfaces/Iterator.ts
@@ -2,8 +2,10 @@ import { Optional_Value } from "../data/Optional_Value"
 import { Abort } from "./Abort"
 
 export type Iterator<Element> = {
-    'look': () => Optional_Value<Element>,
-    'look ahead': (offset: number) => Optional_Value<Element>
+    'has next': () => boolean,
+    'look': () => Element,
+    'has ahead': (offset: number) => boolean,
+    'look ahead': (offset: number) => Element
     'consume': <T>(
         callback: (value: Element, position: number) => T,
         abort: Abort<number>
